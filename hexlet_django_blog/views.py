2fd8273c2c5dbebcd5881f71006a6a5a67a5b3f9
Elena Dobruk <elena.dobruk@yandex.ru>
2024-06-27 17:05:24 +0300

    add new page /about

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -5,3 +5,7 @@ def index(request):
     return render(request, 'index.html', context={
         'who': 'the Most Esteemed Visitor',
     })
+
+
+def about(request):
+    return render(request, 'about.html')
