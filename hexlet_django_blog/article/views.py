c469bb32b8df42d38771b46e63faf73b18d1036f
Elena Dobruk <elena.dobruk@yandex.ru>
2024-07-01 17:16:02 +0300

    step7.2: edit article.views by replacing index() with the new class IndexView

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 
-def index(request):
-    return render(request, 'articles/index.html')
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'articles/index.html')
