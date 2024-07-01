4f552f75946164716a19eb8c9a9c0861a9a420f9
Elena Dobruk <elena.dobruk@yandex.ru>
2024-07-01 17:23:16 +0300

    step7: edit views.py by replacing index() with the new class HomepageView

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'the Most Esteemed Visitor',
-    })
+class HomepageView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'the Most Esteemed Visitor'
+        return context
 
 
 def about(request):
